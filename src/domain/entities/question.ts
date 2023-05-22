07382df38092dd591940c7125fdc4cbd6f3f1f27
Antonio Bernardino da Silva <antoniobsilvaprof@hotmail.com>
2023-05-22 14:57:01 -0300

    feat: add path aliases and vitest config

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,7 @@
 import { Slug } from "./values-objects/slug";
-import { Entity } from "../../core/entities/entity";
-import { UniqueEntityID } from "../../core/entities/unique-entity-id";
-import { Optional } from "../../core/types/optional";
+import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+import { Optional } from "@/core/types/optional";
 import dayjs from "dayjs";
 
 interface QuestionProps {
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  plugins: [
+    tsConfigPaths()
+  ],
+  test: {
+    globals: true
+  }
+})
